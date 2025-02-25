33a0d427c73107deff833191ee0693c1b4108e80
AncientGreekProgrammer <Iwannis123@outlook.com.gr>
2025-02-25 19:46:57 +0100

    TreeNode's refactored to records

--- book-app/src/main/java/gr/bookapp/storage/codec/TreeNode.java
+++ book-app/src/main/java/gr/bookapp/storage/codec/TreeNode.java
@@ -1,18 +1,9 @@
 package gr.bookapp.storage.codec;
 
-public class TreeNode<K> {
-
-    public K key;
-    public long leftPointer;
-    public long rightPointer;
+public record TreeNode<K>(K key, long leftPointer, long rightPointer) {
 
     public TreeNode(K key) {
-        this.key = key;
+        this(key, 0, 0);
     }
 
-    public TreeNode(K key, long leftPointer, long rightPointer) {
-        this.key = key;
-        this.leftPointer = leftPointer;
-        this.rightPointer = rightPointer;
-    }
 }
--- book-app/src/main/java/gr/bookapp/storage/codec/TreeNodeDual.java
+++ book-app/src/main/java/gr/bookapp/storage/codec/TreeNodeDual.java
@@ -1,21 +1,9 @@
 package gr.bookapp.storage.codec;
 
-public final class TreeNodeDual<K,V> {
-
-    public K key;
-    public long leftPointer;
-    public long rightPointer;
-    public V value;
+public record TreeNodeDual<K,V>(K key, long leftPointer, long rightPointer, V value) {
 
     public TreeNodeDual(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public TreeNodeDual(K key, long leftPointer, long rightPointer, V value) {
-        this(key,value);
-        this.leftPointer = leftPointer;
-        this.rightPointer = rightPointer;
+        this(key, 0, 0, value);
     }
 
     public TreeNodeDual(TreeNode<K> keyNode, V value) {
