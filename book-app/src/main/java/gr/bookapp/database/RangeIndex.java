ee84c364e91c06cbf7a53227d7e37f304182d1cd
AncientGreekProgrammer <Iwannis123@outlook.com.gr>
2025-03-10 20:09:07 +0100

    rebased from main

--- book-app/src/main/java/gr/bookapp/database/RangeIndex.java
+++ book-app/src/main/java/gr/bookapp/database/RangeIndex.java
@@ -1,25 +1,7 @@
 package gr.bookapp.database;
 
-import java.util.Comparator;
-import java.util.function.Function;
+import java.util.Iterator;
 
 public interface RangeIndex<T,K> extends Index<T, K>{
-
-    static <T, K> RangeIndex<T, K> of(Function<T, K> extractKey,
-                                      Comparator<K> comparator){
-        return new RangeIndex<T, K>() {
-            @Override
-            public Comparator<K> comparator() {
-                return comparator;
-            }
-
-            @Override
-            public K extractKey(T obj) {
-                return extractKey.apply(obj);
-            }
-        };
-    }
-
-    Comparator<K> comparator();
-
+    Iterator<T> objectsInRange(K min, K max);
 }
---
+++ book-app/src/main/java/gr/bookapp/repositories/AuthenticationRepository.java
@@ -0,0 +1,8 @@
+package gr.bookapp.repositories;
+
+import gr.bookapp.database.Database;
+
+public final class AuthenticationRepository {
+
+    Database< >
+}
