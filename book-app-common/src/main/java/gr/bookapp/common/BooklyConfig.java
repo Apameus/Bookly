123cca585367b3fead40d3fa1982946f6e7c4138
AncientGreekProgrammer <Iwannis123@outlook.com.gr>
2025-04-19 06:36:20 +0200

    - Add SocketAddress to BooklyConfig & ConfigLoader

--- book-app-common/src/main/java/gr/bookapp/common/BooklyConfig.java
+++ book-app-common/src/main/java/gr/bookapp/common/BooklyConfig.java
@@ -1,7 +1,8 @@
-package gr.bookapp.common;
+package gr.bookapp.common; // TODO: Relocate to database module ?
 
+import java.net.SocketAddress;
 import java.nio.file.Path;
 
-public record BooklyConfig(Path booksPath, Path bookSalesPath, Path employeesPath, Path offersPath, Path auditsPath, Path logsPath) {
+public record BooklyConfig(Path booksPath, Path bookSalesPath, Path userPath, Path offersPath, Path auditsPath, Path logsPath, SocketAddress address) {
 
 }
--- book-app-common/src/main/java/gr/bookapp/common/ConfigLoader.java
+++ book-app-common/src/main/java/gr/bookapp/common/ConfigLoader.java
@@ -3,15 +3,17 @@ package gr.bookapp.common;
 import gr.bookapp.exceptions.ConfigurationFileLoadException;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Properties;
 
-public final class ConfigLoader {
+public final class ConfigLoader { //TODO: Refactor from Path -> Properties
     private final BooklyConfig booklyConfig;
 
-    public ConfigLoader(String pathOfConfigFile) throws ConfigurationFileLoadException {
+    public ConfigLoader(String pathOfConfigFile) throws ConfigurationFileLoadException, IOException {
 
         Path dataDirectory = loadDataDirectory(pathOfConfigFile);
 
@@ -21,9 +23,16 @@ public final class ConfigLoader {
         Path offers = dataDirectory.resolve("Offers");
         Path audits = dataDirectory.resolve("Audits");
         Path logs = dataDirectory.resolve("Logs");
+        SocketAddress address = loadAddress(dataDirectory.resolve("Address"));
         ensureExistenceOfFiles(books, bookSales, users, offers, audits, logs);
 
-        booklyConfig = new BooklyConfig(books, bookSales, users, offers, audits, logs);
+        booklyConfig = new BooklyConfig(books, bookSales, users, offers, audits, logs, address);
+    }
+
+    private SocketAddress loadAddress(Path dataDirectory) throws IOException {
+        String[] parts = Files.readString(dataDirectory).trim().split(":");
+        if (parts.length != 2) throw new IllegalStateException("Invalid Socket Address format. Expected 'host:port'");
+        return new InetSocketAddress(parts[0], Integer.parseInt(parts[1]));
     }
 
     private static Path loadDataDirectory(String pathOfConfigFile) throws ConfigurationFileLoadException {
